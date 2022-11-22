17f2623c85e0ca326e7cd82626726450aa30ba50
iali <engr.imran_1242@hotmail.com>
2022-11-22 14:44:17 +0100

    complete Blop App

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const methodOverrise = require('method-override')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverrise = require("method-override");
+const Article = require("./models/article");
+const articleRouter = require("./routes/articles");
+require("dotenv").config();
 
-const app = express()
-mongoose.connect('mongodb://localhost/myBlog')
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static(__dirname + '/public'))
-app.use(methodOverrise('_method'))
+const app = express();
+mongoose.connect(process.env.DB_URI);
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverrise("_method"));
 // main route
-app.get('/', async (req, res) => {
-  const articles = await Article.find().sort({ createAt: 'desc' })
-  res.render('articles/index', { articles: articles })
-})
-app.use('/articles', articleRouter)
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
+app.use("/articles", articleRouter);
 // localhost port
-app.listen(3000)
+// app.listen(3000)
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://loccalhost:${process.env.PORT}`);
+});
